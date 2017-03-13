d198aff8b6d12e09db9dd6a6b59202d4831c55a8
Sergey Seroukhov <seroukhov@gmail.com>
2017-03-13 10:54:20 -0700

    Rebuilt

--- dist/pip-webui-files.d.ts
+++ dist/pip-webui-files.d.ts
@@ -10,7 +10,6 @@ export class ButtonsUpload {
 
 
 
-
 export enum FileUploadState {
     Uploading = 0,
     Completed = 1,
@@ -20,6 +19,7 @@ export enum FileUploadState {
 export interface IFileUploadService {
     upload(file: any, url: string, resultCallback?: (data: any, err: any) => void, progressCallback?: (state: FileUploadState, progress: number) => void): any;
 }
+
 
 
 
